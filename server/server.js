74ada805f3d0fc1ab204b2b0b520e1d30efdaf8b
iamritikpal <itsritikpal@gmail.com>
2026-03-07 18:58:46 +0530

    updating cors

--- server/server.js
+++ server/server.js
@@ -19,6 +19,8 @@ const allowedOrigins = [
   'https://voicefluence-proxy-73gqulicu-ritiks-projects-c9bc00c2.vercel.app',
   'https://voicefluence-proxy.vercel.app',
   'http://app.postflux.tech',
+  'https://app.postflux.tech',
+  'http://postflux.tech',
   'https://postflux.tech',
   process.env.CLIENT_URL,
 ].filter(Boolean);
